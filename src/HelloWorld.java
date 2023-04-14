d0e083e37fabc7e96e8ad2da36b456d98b037e8c
bincoi03 <89581154+bincoi03@users.noreply.github.com>
2023-04-14 14:20:30 +0700

    Delete src directory

--- src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String args[]){
-        System.out.println("Le Minh Phuc - 20210687");
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
